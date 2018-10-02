af075abdb76be37109f1acaffb8bbb8b2297946f
Aditya Agarwal <aditya81070@gmail.com>
2018-10-02 14:23:26 +0530

    BookShelf.js: Add shleves component to display books

---
+++ src/BookShelf.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import Book from './Book'
+import PropTypes from 'prop-types'
+
+class BookShelf extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    books: PropTypes.array.isRequired,
+    onChangeShelf: PropTypes.func.isRequired
+  }
+  
+  updateBook = (book, shelf) => {
+    this.props.onChangeShelf(book,shelf)
+  }
+  render () {
+    const books = this.props.books
+    return (
+      <div className='bookshelf'>
+              <h2 className='bookshelf-title'>{this.props.title}</h2>
+              <div className='bookshelf-books'>
+                <ol className='books-grid'>
+                  {
+                    books.map((book, index) => (<Book book={book} key={index} onUpdate={(shelf) => (this.updateBook(book, shelf))} /> ))
+                  }
+                </ol>
+              </div>
+      </div>
+    )
+  }
+}
+
+export default BookShelf
