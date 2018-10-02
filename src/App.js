d6158ce07fca6ac5e107c073ffba175cfaf9149c
Aditya Agarwal <aditya81070@gmail.com>
2018-10-02 11:37:41 +0530

    Fetch data from api and update book function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import { Route } from 'react-router-dom'
 import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
 import './App.css'
 
 class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+  
+  componentDidMount () {
+    this.fetchBookDetails()
+  }
+
+  fetchBookDetails = () => {
+    BooksAPI.getAll()
+      .then((books) => {
+        this.setState({ books })
+      })
+  }
+
+  updateBookDetails = (book, shelf) => {
+    BooksAPI.update(book, shelf)
+      .then((params) => {
+        this.fetchBookDetails()
+      })
+
+  }
+  
   render () {
     return (
       <div className='app'>
