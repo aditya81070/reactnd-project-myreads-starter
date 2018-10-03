ce7b6390b26a35a37a331094f7065cc80040f504
Aditya Agarwal <aditya81070@gmail.com>
2018-10-03 20:22:07 +0530

    Remove unused classes

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -3,14 +3,10 @@ import { Link } from 'react-router-dom'
 
 function NotFound () {
   return (
-    <div id='notfound'>
-      <div class='notfound'>
-        <div class='notfound-404'>
-          <h1>Oops!</h1>
-          <h2>404 - The Book not found</h2>
-        </div>
-        <Link to='/'>Go To Homepage</Link>
-      </div>
+    <div>
+      <h1>Oops!</h1>
+      <h2>404 - The Book not found</h2>
+      <Link to='/'>Go To Homepage</Link>
     </div>
   )
 }
