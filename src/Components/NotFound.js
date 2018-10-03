97a6eec60e24473c80e2c2c19092d274a3fce612
Aditya Agarwal <aditya81070@gmail.com>
2018-10-03 15:37:33 +0530

    Remove unnecessary prop argument

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function NotFound (props) {
+function NotFound () {
   return (
     <div id='notfound'>
       <div class='notfound'>
