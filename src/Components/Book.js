14ebe61310ee064f96caf656e4022ccc0d2035f7
Aditya Agarwal <aditya81070@gmail.com>
2018-10-03 20:13:47 +0530

    Convert component to stateless component

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,41 +1,39 @@
-import React, { Component } from 'react'
-import PropTypes  from 'prop-types'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-class Book extends Component {
-  static propTypes = {
-    book: PropTypes.object.isRequired,
-    onUpdate: PropTypes.func.isRequired
+const Book = ({ book, onUpdate }) => {
+  const changeBookShelf = (shelf) => {
+    onUpdate(shelf)
   }
-  changeBookShelf = (shelf) => {
-    this.props.onUpdate(shelf)
-  }
-  render () {
-    const book = this.props.book
-    return (
-      <li>
-        <div className='book'>
-          <div className='book-top'>
-            <div className='book-cover'
-              style={{ 
-                width: 128,
-                height: 193,
-                backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
-            <div className='book-shelf-changer'>
-              <select onChange={(e) => this.changeBookShelf(e.target.value)} value={book.shelf}>
-                <option value='move' disabled>Move to...</option>
-                <option value='currentlyReading'>Currently Reading</option>
-                <option value='wantToRead'>Want to Read</option>
-                <option value='read'>Read</option>
-                <option value='none'>None</option>
-              </select>
-            </div>
+
+  return (
+    <li>
+      <div className='book'>
+        <div className='book-top'>
+          <div className='book-cover'
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
+          <div className='book-shelf-changer'>
+            <select onChange={(e) => changeBookShelf(e.target.value)} value={book.shelf}>
+              <option value='move' disabled>Move to...</option>
+              <option value='currentlyReading'>Currently Reading</option>
+              <option value='wantToRead'>Want to Read</option>
+              <option value='read'>Read</option>
+              <option value='none'>None</option>
+            </select>
           </div>
-          <div className='book-title'>{book.title}</div>
-          <div className='book-authors'>{book.authors}</div>
         </div>
-      </li>
-    )
-  }
+        <div className='book-title'>{book.title}</div>
+        <div className='book-authors'>{book.authors}</div>
+      </div>
+    </li>
+  )
+}
+Book.propTypes = {
+  book: PropTypes.object.isRequired,
+  onUpdate: PropTypes.func.isRequired
 }
 
 export default Book
